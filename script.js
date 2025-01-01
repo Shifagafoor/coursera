52e64defe30460a0cfe554ab02b4169fe6ab52cc
Shifa Gafoor A <shifagafoorncas@gmail.com>
2025-01-01 14:38:09 +0530

    .

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ arrows.forEach(arrow => {
         arrow.parentNode.classList.toggle('show')
     })
 })
+
+const filterClick = document.querySelector('.whole_filter')
+const filter_two = document.querySelector('.filter_two')
+
+filterClick.addEventListener('click', () => {
+    console.log('click')
+        filter_two.classList.toggle('filter_two_active')
+}) 
